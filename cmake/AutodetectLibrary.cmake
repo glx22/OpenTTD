0af9da9da2e205c0b81c2c63d3073527f557a45b
Patric Stout <truebrain@openttd.org>
2019-03-02 17:00:22 +0100

    Fix: patch vcpkg results to be more consistent in 'optimized/debug'
    By default, 'debug' is on top of the search path. In result, even
    release builds picked up the debug libraries. This of course is not
    what we want. When using vcpkg now, it always creates optimized/debug
    libraries, hopefully fixing the issue.

--- cmake/AutodetectLibrary.cmake
+++ cmake/AutodetectLibrary.cmake
@@ -29,6 +29,61 @@ macro(_autodetect_library_via_find NAME HEADER_PATH HEADER LIBRARY)
     endif (${NAME}_INCLUDE_DIR AND ${NAME}_LIBRARY)
 endmacro()
 
+macro(_patch_older_cmake NAME)
+    # Patch up libraries that still announce under the deprecated name only
+    if (NOT ${NAME}_LIBRARIES)
+        set(${NAME}_LIBRARIES "${${NAME}_LIBRARY}")
+    endif (NOT ${NAME}_LIBRARIES)
+    if (NOT ${NAME}_INCLUDE_DIRS)
+        set(${NAME}_INCLUDE_DIRS "${${NAME}_INCLUDE_DIR}")
+    endif (NOT ${NAME}_INCLUDE_DIRS)
+endmacro()
+
+macro(_patch_vcpkg_debug_optimized NAME)
+    # With vcpkg, the library path should contain both 'debug' and 'optimized'
+    # entries (see target_link_libraries() documentation for more information)
+    #
+    # Some libraries do this, but not all. In cases where this goes wrong,
+    # we will fix things up ourself.
+    #
+    # NOTE: we only patch up when using vcpkg; the same issue might happen
+    # when not using vcpkg, but this is non-trivial to fix, as we have no idea
+    # what the paths are. With vcpkg we do. And we only official support vcpkg
+    # with Windows.
+    #
+    # NOTE: this is based on the assumption that the debug file has the same
+    # name as the optimized file. This is not always the case, but so far
+    # experiences has shown that in those case vcpkg CMake files do the right
+    # thing.
+
+    if (VCPKG_TOOLCHAIN AND ${NAME}_LIBRARIES)
+        # Is this entry already of the correct format?
+        string(FIND "${${NAME}_LIBRARIES}" "optimized;" HAS_OPTIMIZED)
+        set(PATCH_LIBRARIES "")
+
+        if (HAS_OPTIMIZED LESS 0)
+            foreach(PATCH_LIBRARY ${${NAME}_LIBRARIES})
+                # Check if we have a 'debug' entry or an 'optimized' entry, and
+                # create the complementary entry.
+                if (${PATCH_LIBRARY} MATCHES "/debug/")
+                    set(PATCH_LIBRARY_DEBUG "${PATCH_LIBRARY}")
+                    string(REPLACE "/debug/lib/" "/lib/" PATCH_LIBRARY_RELEASE "${PATCH_LIBRARY}")
+                else (${LIBRARY} MATCHES "/debug/")
+                    set(PATCH_LIBRARY_RELEASE "${PATCH_LIBRARY}")
+                    string(REPLACE "/lib/" "/debug/lib/" PATCH_LIBRARY_DEBUG "${PATCH_LIBRARY}")
+                endif (${PATCH_LIBRARY} MATCHES "/debug/")
+
+                LIST(APPEND PATCH_LIBRARIES
+                    "optimized" "${PATCH_LIBRARY_RELEASE}"
+                    "debug" "${PATCH_LIBRARY_DEBUG}")
+            endforeach()
+
+            # Reassembly a single variable that sets both
+            set(${NAME}_LIBRARIES ${PATCH_LIBRARIES})
+        endif (HAS_OPTIMIZED LESS 0)
+    endif (VCPKG_TOOLCHAIN AND ${NAME}_LIBRARIES)
+endmacro()
+
 macro(autodetect_library FRIENDLY NAME PACKAGE PKGCONFIG HEADER_PATH HEADER LIBRARY)
     message(STATUS "Detecting ${FRIENDLY}")
 
@@ -44,13 +99,8 @@ macro(autodetect_library FRIENDLY NAME PACKAGE PKGCONFIG HEADER_PATH HEADER LIBR
     endif (NOT ${NAME}_FOUND)
 
     if (${NAME}_FOUND)
-        # Patch up libraries that still announce under the deprecated name only
-        if (NOT ${NAME}_LIBRARIES)
-            set(${NAME}_LIBRARIES "${${NAME}_LIBRARY}")
-        endif (NOT ${NAME}_LIBRARIES)
-        if (NOT ${NAME}_INCLUDE_DIRS)
-            set(${NAME}_INCLUDE_DIRS "${${NAME}_INCLUDE_DIR}")
-        endif (NOT ${NAME}_INCLUDE_DIRS)
+        _patch_older_cmake("${NAME}")
+        _patch_vcpkg_debug_optimized("${NAME}")
 
         message(STATUS "${NAME}_INCLUDE_DIRS: ${${NAME}_INCLUDE_DIRS}")  # TODO: temporary debug
         message(STATUS "${NAME}_LIBRARIES: ${${NAME}_LIBRARIES}") # TODO: temporary debug
