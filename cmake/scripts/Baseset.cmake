6ed365c4d8ac6a1e700ad0e83fd183e17ffe9a83
glx <glx@openttd.org>
2019-03-07 03:28:58 +0100

    Change: use the same file list for language compilation and baseset generation

--- cmake/scripts/Baseset.cmake
+++ cmake/scripts/Baseset.cmake
@@ -3,17 +3,17 @@ cmake_minimum_required(VERSION 3.5)
 # Retrieve the arguments (0 to 2 are for "cmake -P Baseset.cmake")
 set(INPUTFILE ${CMAKE_ARGV3})
 set(OUTPUTFILE ${CMAKE_ARGV4})
-set(LANGDIR ${CMAKE_ARGV5})
+set(LANGFILES ${CMAKE_ARGV5})
 set(ORIG_EXTRA_GRF ${CMAKE_ARGV6})
+
+# Convert the command line compatible string into a real list
+string(REPLACE "\\ " ";" LANGFILES ${LANGFILES})
 
 # Place holder format is @<ini_key>_<str_id>@
 file(STRINGS ${INPUTFILE} PLACE_HOLDER REGEX "^@")
 string(REGEX REPLACE "@([^_]+).*@" "\\1" INI_KEY "${PLACE_HOLDER}")
 string(REGEX REPLACE "@[^_]+_(.*)@" "\\1" STR_ID "${PLACE_HOLDER}")
 string(REGEX REPLACE "@(.*)@" "\\1" PLACE_HOLDER "${PLACE_HOLDER}")
-
-# Get all the (finished) language files
-file(GLOB LANGFILES ${LANGDIR}/*.txt)
 
 # Get the translations
 foreach(LANGFILE IN LISTS LANGFILES)
