9fd5afdb72c384fa3a931f52b846d2cdfc111a3a
Patric Stout <truebrain@openttd.org>
2019-03-11 15:02:09 +0100

    Fix: baseset parameters now work on Linux/Windows, including spaces in folders

--- cmake/scripts/Baseset.cmake
+++ cmake/scripts/Baseset.cmake
@@ -1,22 +1,32 @@
 cmake_minimum_required(VERSION 3.5)
 
-# Retrieve the arguments (0 to 2 are for "cmake -P Baseset.cmake")
-set(INPUTFILE ${CMAKE_ARGV3})
-set(OUTPUTFILE ${CMAKE_ARGV4})
-set(LANGFILES ${CMAKE_ARGV5})
-set(ORIG_EXTRA_GRF ${CMAKE_ARGV6})
+set(ARGC 1)
+set(ARG_READ NO)
 
-# Convert the command line compatible string into a real list
-string(REPLACE "%" ";" LANGFILES ${LANGFILES})
+# Read all the arguments given to CMake; we are looking for -- and everything
+# that follows. Those are our language files.
+while(ARGC LESS CMAKE_ARGC)
+    set(ARG ${CMAKE_ARGV${ARGC}})
+
+    if (ARG_READ)
+        list(APPEND LANG_SOURCE_FILES "${ARG}")
+    endif (ARG_READ)
+
+    if (ARG STREQUAL "--")
+        set(ARG_READ YES)
+    endif (ARG STREQUAL "--")
+
+    math(EXPR ARGC "${ARGC} + 1")
+endwhile()
 
 # Place holder format is @<ini_key>_<str_id>@
-file(STRINGS ${INPUTFILE} PLACE_HOLDER REGEX "^@")
+file(STRINGS "${BASESET_SOURCE_FILE}" PLACE_HOLDER REGEX "^@")
 string(REGEX REPLACE "@([^_]+).*@" "\\1" INI_KEY "${PLACE_HOLDER}")
 string(REGEX REPLACE "@[^_]+_(.*)@" "\\1" STR_ID "${PLACE_HOLDER}")
 string(REGEX REPLACE "@(.*)@" "\\1" PLACE_HOLDER "${PLACE_HOLDER}")
 
 # Get the translations
-foreach(LANGFILE IN LISTS LANGFILES)
+foreach(LANGFILE IN LISTS LANG_SOURCE_FILES)
     file(STRINGS "${LANGFILE}" LANGLINES REGEX "^(##isocode|${STR_ID})" ENCODING UTF-8)
     string(FIND "${LANGLINES}" "${STR_ID}" HAS_STR_ID)
     if (HAS_STR_ID LESS 0)
@@ -34,6 +44,6 @@ list(SORT ${PLACE_HOLDER})
 string(REPLACE ";" "\n" ${PLACE_HOLDER} "${${PLACE_HOLDER}}")
 
 # Get the grf md5
-file(MD5 ${ORIG_EXTRA_GRF} ORIG_EXTRA_GRF_MD5)
+file(MD5 ${BASESET_EXTRAGRF_FILE} ORIG_EXTRA_GRF_MD5)
 
-configure_file(${INPUTFILE} ${OUTPUTFILE})
+configure_file(${BASESET_SOURCE_FILE} ${BASESET_BINARY_FILE})
