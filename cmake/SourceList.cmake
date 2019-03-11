f915cc6502880a7158370c8f01194ef49177e60a
Patric Stout <truebrain@openttd.org>
2019-03-11 18:09:46 +0100

    Change: rework how we deal with source_files
    add_files() now has CONDITION, which allows for quick filtering.
    This makes a lot of the structures more human readable.
    Also switched to target_sources() instead of a custom list.
    Lastly, COMPILE_FLAGS can be added now in local files, and still
    be available on global.
    
    While at it, cleaned up CMakeLists.txt where possible.

--- cmake/SourceList.cmake
+++ cmake/SourceList.cmake
@@ -1,149 +1,63 @@
-macro(source_list)
-    if (SDL_FOUND)
-        set(OPTION_SDL YES)
-    else (SDL_FOUND)
-        set(OPTION_SDL NO)
-    endif (SDL_FOUND)
-
-    if (ALLEGRO_FOUND)
-        set(OPTION_ALLEGRO YES)
-    else (ALLEGRO_FOUND)
-        set(OPTION_ALLEGRO NO)
-    endif (ALLEGRO_FOUND)
-
-    if (SSE_FOUND)
-        set(OPTION_USE_SSE YES)
-    else (SSE_FOUND)
-        set(OPTION_USE_SSE NO)
-    endif (SSE_FOUND)
-
-    if (XAUDIO2_FOUND)
-        set(OPTION_USE_XAUDIO2 YES)
-    else (XAUDIO2_FOUND)
-        set(OPTION_USE_XAUDIO2 NO)
-    endif (XAUDIO2_FOUND)
-
-    if (FLUIDSYNTH_FOUND)
-        set(OPTION_FLUIDSYNTH YES)
-    else (FLUIDSYNTH_FOUND)
-        set(OPTION_FLUIDSYNTH NO)
-    endif (FLUIDSYNTH_FOUND)
-
-    set(OPTION_COCOA NO)
-    set(OPTION_OSX NO)
-    set(OPTION_WIN32 NO)
-
-    if (APPLE)
-        set(OPTION_OSX YES)
-        set(OPTION_COCOA YES)
-    elseif (WIN32)
-        set(OPTION_WIN32 YES)
-    endif ()
-
-    # These OSes used to be supported by OpenTTD, but have no CMake code yet
-    set(OPTION_HAIKU NO)
-    set(OPTION_DOS NO)
-    set(OPTION_OS2 NO)
-
-    file(READ ${CMAKE_SOURCE_DIR}/source.list SOURCE_LIST)
-    string(REPLACE "\n" ";" SOURCE_LIST "${SOURCE_LIST}")
-    string(REGEX REPLACE ";\t*" ";" SOURCE_LIST "${SOURCE_LIST}")
-    string(REGEX REPLACE "[;]+;" ";" SOURCE_LIST "${SOURCE_LIST}")
-    string(REGEX REPLACE ";$" "" SOURCE_LIST "${SOURCE_LIST}")
-
-    set(SCAN_SKIP 0)
-    set(SCAN_DEPTH 0)
-    foreach (FILE IN LISTS SOURCE_LIST)
-        if (${FILE} MATCHES "^#end")
-            if (${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-                math(EXPR SCAN_SKIP "${SCAN_SKIP} - 1")
-            endif (${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-
-            math(EXPR SCAN_DEPTH "${SCAN_DEPTH} - 1")
-
-            continue()
-        endif (${FILE} MATCHES "^#end")
-
-        if (${FILE} MATCHES "^#else")
-            if (${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-                math(EXPR SCAN_SKIP "${SCAN_SKIP} - 1")
-            else (${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-                math(EXPR SCAN_DEPTH_PARENT "${SCAN_DEPTH} - 1")
-                if (${SCAN_DEPTH_PARENT} EQUAL ${SCAN_SKIP})
-                    math(EXPR SCAN_SKIP "${SCAN_SKIP} + 1")
-                endif (${SCAN_DEPTH_PARENT} EQUAL ${SCAN_SKIP})
-            endif (${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-
-            continue()
-        endif (${FILE} MATCHES "^#else")
-
-        if (${FILE} MATCHES "^#if")
-            if (NOT ${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-                math(EXPR SCAN_DEPTH "${SCAN_DEPTH} + 1")
-                continue()
-            endif (NOT ${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-
-            math(EXPR SCAN_DEPTH "${SCAN_DEPTH} + 1")
-
-            string(REGEX MATCH "#if (.*)" IF_DEFINE ${FILE})
-            set(IF_DEFINE ${CMAKE_MATCH_1})
-
-            if (DEFINED OPTION_${IF_DEFINE})
-                if (${OPTION_${IF_DEFINE}})
-                    math(EXPR SCAN_SKIP "${SCAN_SKIP} + 1")
-                endif (${OPTION_${IF_DEFINE}})
-                continue()
-            endif (DEFINED OPTION_${IF_DEFINE})
-
-            message(FATAL_ERROR "Define ${IF_DEFINE} is not set")
-        endif (${FILE} MATCHES "^#if")
-
-        # Skip any other line starting with #
-        if (${FILE} MATCHES "^#")
-            continue()
-        endif (${FILE} MATCHES "^#")
-
-        # Skip the line if this is not our scan depth
-        if (NOT ${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-            continue()
-        endif (NOT ${SCAN_DEPTH} EQUAL ${SCAN_SKIP})
-
-        # TODO -- For now, ignore these ugly entries
-        if (${FILE} MATCHES "../objs/*")
-            continue()
-        endif (${FILE} MATCHES "../objs/*")
-
-        list(APPEND SOURCE_FILES ${CMAKE_SOURCE_DIR}/src/${FILE})
-    endforeach(FILE)
-endmacro()
-
-set_property(GLOBAL PROPERTY source_list)
+# Add a file to be compiled.
+#
+# add_files([file1 ...] CONDITION condition [condition ...])
+#
+# CONDITION is a complete statement that can be evaluated with if().
+# If it evaluates true, the source files will be added; otherwise not.
+# For example: ADD_IF SDL_FOUND AND ALLEGRO_FOUND
+#
 function(add_files)
-    get_property(SOURCE_FILES2 GLOBAL PROPERTY source_list)
-    foreach(FILE ${ARGV})
-        if (NOT "${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
-            list(APPEND SOURCE_FILES2 ${CMAKE_CURRENT_SOURCE_DIR}/${FILE})
-        else()
-            list(APPEND SOURCE_FILES2 ${tmp} ${CMAKE_SOURCE_DIR}/src/${FILE})
-        endif()
+    cmake_parse_arguments(PARAM "" "" "CONDITION" ${ARGN})
+    set(PARAM_FILES "${PARAM_UNPARSED_ARGUMENTS}")
+
+    if (PARAM_CONDITION)
+        if (NOT (${PARAM_CONDITION}))
+            return()
+        endif (NOT (${PARAM_CONDITION}))
+    endif (PARAM_CONDITION)
+
+    foreach(FILE ${PARAM_FILES})
+        target_sources(openttd PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/${FILE})
     endforeach()
-    set_property(GLOBAL PROPERTY source_list "${SOURCE_FILES2}")
 endfunction(add_files)
 
-function(CheckSourceEquality)
-	get_property(SOURCE_FILES2 GLOBAL PROPERTY source_list)
+# This function works around an 'issue' with CMake, where
+# set_source_files_properties() only works in the scope of the file. We want
+# to set properties for the source file on a more global level. To solve this,
+# this function records the flags you want, and a macro adds them in the root
+# CMakeLists.txt.
+# See this URL for more information on the issue:
+# http://cmake.3232098.n2.nabble.com/scope-of-set-source-files-properties-td4766111.html
+#
+# set_compile_flags([file1 ...] COMPILE_FLAGS cflag [cflag ...])
+#
+function(set_compile_flags)
+    cmake_parse_arguments(PARAM "" "" "COMPILE_FLAGS" ${ARGN})
+    set(PARAM_FILES "${PARAM_UNPARSED_ARGUMENTS}")
 
-    foreach(FILE ${SOURCE_FILES2})
-        list(FIND SOURCE_FILES "${FILE}" FOUND)
-        if(FOUND LESS 0)
-            message(STATUS "${FILE} only in SOURCE_FILES2")
-        endif(FOUND LESS 0)
+    get_property(SOURCE_PROPERTIES GLOBAL PROPERTY source_properties)
+
+    foreach(FILE ${PARAM_FILES})
+        list(APPEND SOURCE_PROPERTIES "${CMAKE_CURRENT_SOURCE_DIR}/${FILE}:${PARAM_COMPILE_FLAGS}")
     endforeach()
 
-    foreach(FILE ${SOURCE_FILES})
-        list(FIND SOURCE_FILES2 "${FILE}" FOUND)
-        if(FOUND LESS 0)
-            message(STATUS "${FILE} only in SOURCE_FILES")
-        endif(FOUND LESS 0)
+    set_property(GLOBAL PROPERTY source_properties "${SOURCE_PROPERTIES}")
+endfunction(set_compile_flags)
+
+# Call this macro in the same CMakeLists.txt and after add_executable().
+# This makes sure all the COMPILE_FLAGS of set_compile_flags() are set
+# correctly.
+#
+# process_compile_flags()
+#
+function(process_compile_flags)
+    get_property(SOURCE_PROPERTIES GLOBAL PROPERTY source_properties)
+
+    foreach(ENTRY ${SOURCE_PROPERTIES})
+        string(REPLACE ":" ";" ENTRY "${ENTRY}")
+        list(GET ENTRY 0 FILE)
+        list(GET ENTRY 1 PROPERTIES)
+
+        set_source_files_properties(${FILE} PROPERTIES COMPILE_FLAGS ${PROPERTIES})
     endforeach()
-endfunction(CheckSourceEquality)
+endfunction(process_compile_flags)
