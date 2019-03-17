c258d012b9a3f020bbc676f0db6b5e18281d117e
Patric Stout <truebrain@openttd.org>
2019-03-17 17:02:01 +0100

    Fix: autocompile grfs if tools are installed

--- cmake/CreateGrfCommand.cmake
+++ cmake/CreateGrfCommand.cmake
@@ -4,6 +4,7 @@ function(create_grf_command)
     get_filename_component(GRF_SOURCE_FOLDER_NAME "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
     get_filename_component(GRF_BINARY_FILE ${CMAKE_CURRENT_SOURCE_DIR}/../${GRF_SOURCE_FOLDER_NAME}.grf ABSOLUTE)
     file(GLOB_RECURSE GRF_PNG_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.png)
+    file(GLOB_RECURSE GRF_NFO_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/*.nfo)
     set(GRF_PNG_SOURCE_FILES ${GRF_PNG_SOURCE_FILES} ${EXTRA_PNG_SOURCE_FILES})
 
     # Copy over all the PNG files to the correct folder
@@ -29,6 +30,7 @@ function(create_grf_command)
                     -DNFORENUM_EXECUTABLE=${NFORENUM_EXECUTABLE}
                     -DGRFCODEC_EXECUTABLE=${GRFCODEC_EXECUTABLE}
                     -P ${CMAKE_SOURCE_DIR}/cmake/scripts/CreateGRF.cmake
+            MAIN_DEPENDENCY ${GRF_NFO_SOURCE_FILES}
             DEPENDS ${GRF_PNG_BINARY_FILES}
                     ${CMAKE_SOURCE_DIR}/cmake/scripts/CreateGRF.cmake
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
